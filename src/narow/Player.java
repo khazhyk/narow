b144ec11b9c9dc377d1e31454e982f6899557c9d
Khazhismel Kumykov <me@khaz.io>
2015-01-19 16:41:44 -0500

    yay

--- src/narow/Player.java
+++ src/narow/Player.java
@@ -1,8 +1,6 @@
 package narow;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Random;
 
 class Player {
@@ -10,22 +8,29 @@ class Player {
 	Config config;
 	String playerName = Long.toHexString(new java.security.SecureRandom().nextLong());
 	Random random = new Random();
+	int ourNumber;
 	
 	void makeMove(String move) throws IOException {
 		
-		List<String> ls=Arrays.asList(move.split(" "));
-		if(ls.size()==2){
+		String[] ls =move.split(" ");
+		if(ls.length==2){
 			System.out.println(random.nextInt(config.width) + " 1");
 		}
-		else if(ls.size()==1){
+		else if(ls.length==1){
 			System.out.println("game over!!!");
 		}
-		else if(ls.size()==5){          //ls contains game info
+		else if(ls.length==5){          //ls contains game info
 			config = new Config(move);
-			System.out.println("0 1");  //first move
+			if (config.player == ourNumber) {
+				System.out.println("0 1");  //first move
+			}
 		}
-		else if(ls.size()==4){		//player1: aa player2: bb
-			//TODO combine this information with game information to decide who is the first player
+		else if(ls.length==4){		//player1: aa player2: bb
+			if (ls[1].equals(playerName)) {
+				ourNumber = 1;
+			} else {
+				ourNumber = 2;
+			}
 		}
 		else
 			System.out.println("not what I want");
