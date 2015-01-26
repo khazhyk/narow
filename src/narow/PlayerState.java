c1463cf53b188a7a144d7a213503df682041f0e8
Khazhismel Kumykov <me@khazhyk.net>
2015-01-26 16:41:27 -0500

    oops

--- src/narow/PlayerState.java
+++ src/narow/PlayerState.java
@@ -3,10 +3,6 @@ package narow;
 import java.io.IOException;
 import java.util.List;
 import java.util.Random;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-
-import javafx.concurrent.Worker.State;
 
 class PlayerState {
 	
