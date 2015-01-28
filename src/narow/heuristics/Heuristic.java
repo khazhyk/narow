11a62978b3f56c568b6e65ac0dfbdd25f2b8efeb
Khazhismel Kumykov <me@khazhyk.net>
2015-01-27 20:20:30 -0500

    comment

--- src/narow/heuristics/Heuristic.java
+++ src/narow/heuristics/Heuristic.java
@@ -3,5 +3,10 @@ package narow.heuristics;
 import narow.BoardState;
 
 public interface Heuristic {
+    /**
+     * 
+     * @param bs
+     * @return MUST return Integer.MAX_VALUE for win state, Integer.MIN_VALUE for lose state. Otherwise, return a score.
+     */
     public int calculate(BoardState bs);
 }
