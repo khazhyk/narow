352e22cbda53c20d5de2112b2daea5b35f95766f
lilwalker <lgwalker@wpi.edu>
2015-01-27 21:18:31 -0500

    HVal2 with terminal test done

---
+++ test/narow/HVal2Test.java
@@ -0,0 +1,153 @@
+package narow;
+
+import static org.junit.Assert.assertEquals;
+import narow.heuristics.CountNARowsCalculator;
+import narow.heuristics.HVal2Calculator;
+import narow.heuristics.Heuristic;
+import narow.state.Action;
+import narow.state.Config;
+import narow.state.Player;
+
+import org.junit.Test;
+
+
+public class HVal2Test {
+
+    @Test
+    public void testGame() {
+    	
+    	BoardState board = new BoardState(
+    			"0 0 0 0",
+                "0 0 0 0",
+                "0 0 0 0",
+                "0 0 0 0"
+                );
+    	
+        Config c = new Config("4 4 4 1 15");
+        
+        Heuristic h = new HVal2Calculator(c);
+        
+        assertEquals(10, h.calculate(board));
+    }
+    
+    @Test
+    public void testGame2() {
+    	
+    	BoardState board = new BoardState(
+    			"0 0 0 0",
+                "0 0 0 0",
+                "0 0 0 0",
+                "0 1 0 0"
+                );
+    	
+        Config c = new Config("4 4 4 1 15");
+        
+        Heuristic h = new HVal2Calculator(c);
+        
+        assertEquals(10, h.calculate(board));
+    }
+    
+    @Test
+    public void testGame3() {
+    	
+    	BoardState board = new BoardState(
+    			"0 0 0 0",
+                "0 0 0 0",
+                "0 0 0 0",
+                "0 2 0 0"
+                );
+    	
+        Config c = new Config("4 4 4 1 15");
+        
+        Heuristic h = new HVal2Calculator(c);
+        
+        assertEquals(8, h.calculate(board));
+    }
+    
+    @Test
+    public void testGame4() {
+    	
+    	BoardState board = new BoardState(
+    			"0 0 0 0",
+                "0 0 0 0",
+                "0 0 0 0",
+                "1 1 1 1"
+                );
+    	
+        Config c = new Config("4 4 4 1 15");
+        
+        Heuristic h = new HVal2Calculator(c);
+        
+        assertEquals(Integer.MAX_VALUE, h.calculate(board));
+    }
+    
+    @Test
+    public void testGame5() {
+    	
+    	BoardState board = new BoardState(
+    			"0 0 0 0",
+                "0 0 0 0",
+                "0 0 0 0",
+                "2 2 2 2"
+                );
+    	
+        Config c = new Config("4 4 4 1 15");
+        
+        Heuristic h = new HVal2Calculator(c);
+        
+        assertEquals(Integer.MIN_VALUE, h.calculate(board));
+    }
+    
+    @Test
+    public void testGame6() {
+    	
+    	BoardState board = new BoardState(
+    			"0 0 0 2",
+                "0 0 2 0",
+                "0 2 0 0",
+                "2 1 2 2"
+                );
+    	
+        Config c = new Config("4 4 4 1 15");
+        
+        Heuristic h = new HVal2Calculator(c);
+        
+        assertEquals(Integer.MIN_VALUE, h.calculate(board));
+    }
+    
+    @Test
+    public void testGame7() {
+    	
+    	BoardState board = new BoardState(
+    			"2 0 0 0",
+                "2 0 0 0",
+                "2 0 0 0",
+                "2 1 2 2"
+                );
+    	
+        Config c = new Config("4 4 4 1 15");
+        
+        Heuristic h = new HVal2Calculator(c);
+        
+        assertEquals(Integer.MIN_VALUE, h.calculate(board));
+    }
+    
+    @Test
+    public void testGame8() {
+    	
+    	BoardState board = new BoardState(
+    			"2 0 0 0",
+                "0 2 0 0",
+                "0 0 2 0",
+                "2 2 1 2"
+                );
+    	
+        Config c = new Config("4 4 4 1 15");
+        
+        Heuristic h = new HVal2Calculator(c);
+        
+        assertEquals(Integer.MIN_VALUE, h.calculate(board));
+    }
+    
+    
+}
